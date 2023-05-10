0282359a3047f995e6b723cd902d682a686df585
Elena Robu <elenarobu@astuntechnology.com>
2023-05-10 12:36:10 +0100

    extended filtering options and improved organisation and license filters

--- libs/feature/search/src/lib/utils/service/fields.service.ts
+++ libs/feature/search/src/lib/utils/service/fields.service.ts
@@ -15,24 +15,22 @@ import {
 })
 export class FieldsService {
   private fields = {
-    publisher: new SimpleSearchField('OrgForResource', 'asc', this.injector),
+    publisher: new SimpleSearchField('OrgForResourceObject.default', 'asc', this.injector),
     format: new SimpleSearchField('format', 'asc', this.injector),
-    publicationYear: new SimpleSearchField(
-      'publicationYearForResource',
-      'desc',
-      this.injector
-    ),
-    topic: new TopicSearchField(this.injector),
+    publicationYear: new SimpleSearchField('publicationYearForResource', 'desc', this.injector),
+    topic: new SimpleSearchField('cl_topic.default', 'asc', this.injector),
+    inspireKeyword: new SimpleSearchField('th_httpinspireeceuropaeutheme-theme_tree.default', 'asc', this.injector),
+    documentStandard: new SimpleSearchField('documentStandard', 'asc', this.injector),
     isSpatial: new IsSpatialSearchField(this.injector),
     q: new FullTextSearchField(),
-    license: new LicenseSearchField(this.injector),
+    license: new SimpleSearchField('licenseObject.default.keyword', 'asc', this.injector),
   } as Record<string, AbstractSearchField>
 
   get supportedFields() {
     return Object.keys(this.fields)
   }
 
-  constructor(private injector: Injector) {}
+  constructor(private injector: Injector) { }
 
   private getField(fieldName: string) {
     if (this.supportedFields.indexOf(fieldName) === -1)
